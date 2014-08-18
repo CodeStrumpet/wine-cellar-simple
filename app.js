b9d92b516111a2d68c7c57d3dd91688d682c2df5
Paul <gidentity@gmail.com>
2014-08-18 04:43:13 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,97 @@
+var app = require('http').createServer(handler), 
+io = require('socket.io').listen(app), 
+fs = require('fs'),
+os = require('os'),
+sp = require("serialport");
+  
+//init for SerialPort connected to Arduino
+var SerialPort = sp.SerialPort
+var serialPort = new SerialPort('/dev/ttyACM0', 
+				{   baudrate: 9600,
+				    dataBits: 8,
+				    parity: 'none',
+				    stopBits: 1,
+				    flowControl: false
+				});
+    
+serialPort.on("open", function () {
+    console.log('serialPort open');
+    serialPort.write("LEDOFF\n");
+});
+    
+//Display my IP
+var networkInterfaces=os.networkInterfaces();
+
+for (var interface in networkInterfaces) {
+    
+    networkInterfaces[interface].forEach(
+        function(details){
+            
+            if (details.family=='IPv4' 
+                && details.internal==false) {
+                console.log(interface, details.address);  
+            }
+	});
+}
+
+//All clients have a common status
+var commonStatus = 'ON';
+
+app.listen(8080);
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+		function (err, data) {
+		    if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		    }
+
+		    res.writeHead(200);
+		    res.end(data);
+		});
+}
+
+io.sockets.on('connection', function (socket) {
+    
+    //Send client with his socket id
+    socket.emit('your id', 
+		{ id: socket.id});
+    
+    //Info all clients a new client caaonnected
+    io.sockets.emit('on connection', 
+		    { client: socket.id,
+		      clientCount: io.sockets.clients().length,
+		    });
+        
+    //Set the current common status to the new client
+    socket.emit('ack button status', { status: commonStatus });
+    
+    socket.on('button update event', function (data) {
+        console.log(data.status);
+        
+        //acknowledge with inverted status, 
+        //to toggle button text in client
+        if(data.status == 'ON'){
+            console.log("ON->OFF");
+            commonStatus = 'OFF';
+            serialPort.write("LEDON\n");
+        }else{
+            console.log("OFF->ON");
+            commonStatus = 'ON';
+            serialPort.write("LEDOFF\n");
+        }
+        io.sockets.emit('ack button status', 
+			{ status: commonStatus,
+			  by: socket.id
+			});
+    });
+    
+    //Info all clients if this client disconnect
+    socket.on('disconnect', function () {
+        io.sockets.emit('on disconnect', 
+			{ client: socket.id,
+			  clientCount: io.sockets.clients().length-1,
+			});
+    });
+});
